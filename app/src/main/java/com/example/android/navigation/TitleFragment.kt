1a25def38eb24960b4cdf2892701f249d0864672
The Aviator <redefinedabhi2@gmail.com>
2021-01-06 16:33:21 +0530

    Adding NavController, Navigation Graph to the app

--- app/src/main/java/com/example/android/navigation/TitleFragment.kt
+++ app/src/main/java/com/example/android/navigation/TitleFragment.kt
@@ -6,6 +6,8 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
+import androidx.navigation.Navigation
+import androidx.navigation.findNavController
 import com.example.android.navigation.databinding.FragmentTitleBinding
 
 class TitleFragment : Fragment() {
@@ -15,6 +17,9 @@ class TitleFragment : Fragment() {
 
         val binding = DataBindingUtil.inflate<FragmentTitleBinding>(inflater,
         R.layout.fragment_title,container,false)
+        binding.playButton.setOnClickListener { view:View ->
+            Navigation.findNavController(view).navigate(R.id.action_titleFragment_to_gameFragment)
+        }
         return binding.root
     }
 }
