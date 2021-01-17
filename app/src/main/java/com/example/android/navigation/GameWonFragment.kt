446b24be6530ffe06745a034b741fa7523b32bb8
The Aviator <redefinedabhi2@gmail.com>
2021-01-17 21:39:27 +0530

    Replaced the action IDs with Navdirections Received the safe args in GameWonFragment Added arguments to the navigation graph to GameWonFragment Action

--- app/src/main/java/com/example/android/navigation/GameWonFragment.kt
+++ app/src/main/java/com/example/android/navigation/GameWonFragment.kt
@@ -20,11 +20,11 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
 import androidx.navigation.findNavController
 import com.example.android.navigation.databinding.FragmentGameWonBinding
-
 
 class GameWonFragment : Fragment() {
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
@@ -35,6 +35,10 @@ class GameWonFragment : Fragment() {
         binding.nextMatchButton.setOnClickListener { view:View ->
             view.findNavController().navigate(R.id.action_gameWonFragment_to_gameFragment)
         }
+
+        val args = GameWonFragmentArgs.fromBundle(requireArguments())
+        Toast.makeText(context,
+                "NumCorrect: ${args.numCorrect}, NumQuestions: ${args.numQuestions}",Toast.LENGTH_LONG).show()
         return binding.root
     }
 }
